5d4fc5b36e3b369fd198c833abb2b21fc09bd331
Ken Goettler <goettlek@gmail.com>
2021-04-03 11:20:20 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ken Goettler on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
